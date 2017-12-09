d2e9025f2e1f2131ca7f80117b5b9f4ffa0f64a1
Matthew Sotoudeh <matthewsot@outlook.com>
2017-12-09 00:24:04 -0800

    Cleaned up a bit, updated README

--- README.md
+++ README.md
@@ -1,7 +1,50 @@
 # docs-vim
-This sort of works. Currently set up to work with my DVORAK layout.
+## Overview
+This simulates very limited versions of vim's insert and normal modes on Google Docs.
 
-* Esc: enter normal mode
-* hn: enter normal mode
-* i: enter insert mode
-* dhtn: move
+Currently supported features:
+* Insert mode:
+    * Typing
+    * Escaping to normal with Esc
+    * Escaping to normal with a two-key combination
+* Normal mode
+    * Getting to insert mode with the "i" key
+    * Moving with the home-row keys
+* Chrome (tested on 63.0)
+* [Not really] Firefox
+    * It will install, but many of the features work incorrectly.
+
+## Keys
+Currently set up to work with my DVORAK layout, these are the default keybindings:
+
+* Insert Mode:
+    * Esc: enter normal mode
+    * hn: enter normal mode
+* Normal Mode:
+    * i: enter insert mode
+    * dhtn: move
+
+Note that most of these can be changed in ``docs-vim.js``. For example, if you want to use the more
+common QWERTY keybindings, you'd change to:
+
+```
+vim = {
+    "mode": "insert",
+    "keys": {
+        "move": "hjkl",
+        "escapeSeq": "jk"
+    }
+};
+```
+
+Near the top of ``docs-vim.js``.
+
+## Future Features
+I don't really use all that much of vim's shortcuts, but a few particular things I want to add
+eventually:
+
+* Selections with v/shift-V (visual/visual line mode)
+* Command mode with find and replace, go to line #, etc.
+* Quick find with /
+* Fixed Firefox support
+
--- docs-vim.js
+++ docs-vim.js
@@ -1,107 +1,83 @@
-var mode = "insert"; // Default to insert mode
-
-function codeFromKey(shifted, key) {
-    var specialKeys = { "/": 191, "Escape": 27, "Shift": 16,
-        "Control": 17, "Alt": 18, "Backspace": 8, " ": 32, "Enter": 13,
-        "Tab": 9, "ArrowLeft": 37, "ArrowUp": 38, "ArrowRight": 39, "ArrowDown": 40,
-    "Delete": 46 };
-    var shiftedSpecialKeys = { "?": 191 };
-    if (typeof specialKeys[key] !== "undefined") {
-        if (shifted && typeof shiftedSpecialKeys[key] !== "undefined") {
-            return shiftedSpecialKeys[key];
-        }
-        return specialKeys[key];
-    } else {
-        // TODO: none of this works correctly yet, but I don't think we use it
-        var c = String.fromCharCode(key);
-        if(!shifted) return c.toLowerCase();
-
-        var shifts = { "`": "~", "1": "!", "2": "@", "3": "#", "4": "$", "5": "%", "6": "^", "7": "&", "8": "*", "9": "(", "0": ")", "-": "_", "=": "+" };
-        var foundShift = shifts[c];
-        if (typeof foundShift === "undefined") {
-            return c.toUpperCase();
-        } else {
-            return shifts[c];
-        }
+vim = {
+    "mode": "insert",
+    "keys": {
+        "move": "dhtn", // QWERTY: hjkl
+        "escapeSeq": "hn" // QWERTY: jk or jl
     }
 };
 
-window.vim_pressed = false;
-docs.pressKey = function (key, keyCode) {
-    function doInsertText(key, keyCode) {
-        var e = docs.utils.createKeyboardEvent("keydown", {
-            "keyCode": keyCode,
-            "key": key
-        });
-
-        document.getElementsByClassName("docs-texteventtarget-iframe")[0].contentWindow.document.querySelector("[contenteditable=\"true\"]").dispatchEvent(e);
-    }
-    window.vim_pressed = true;
-    
-    if (docs.platform !== "userscript") {
-        docs.runWithCreateKeyboard(doInsertText.toString(), "doInsertText", "\"" + key + "\", " + keyCode);
-    } else {
-        doInsertText(key, keyCode);
-    }
+vim.switchToNormalMode = function () {
+    vim.mode = "normal";
+    docs.setCursorWidth("7px");
+    docs.keyboard.startBlockingKeyboard();
 };
 
+vim.switchToInsertMode = function () {
+    vim.mode = "insert";
+    docs.setCursorWidth("2px");
+    docs.keyboard.stopBlockingKeyboard();
+};
+
+// This is what gets called when the keyboard is "blocked"
+// AKA we're in normal mode
 docs.keyboard.handleKeyboard = function (e) {
-    if (window.vim_pressed) {
-        window.vim_pressed = false;
+    // Allow keys that we send to go through even in normal mode
+    if (window.vim_pressed > 0) {
+        window.vim_pressed--;
         return true;
     }
+
     if (typeof e.key === "undefined") {
         e.key = docs.utils.keyFromKeyCode(e.shiftKey, e.keyCode);
     }
 
-    if (mode == "insert") {
+    if (e.key == "i") {
+        vim.switchToInsertMode();
+        return true;
     }
-    if (mode == "normal") {
-        var keyMap = { "d": "ArrowLeft", "h": "ArrowDown", "t": "ArrowUp", "n": "ArrowRight" };
-        if (e.key in keyMap) {
-            e.key = keyMap[e.key];
-        }
-        if (e.key.indexOf("Arrow") == 0
-        || e.key == "Delete") {
-            docs.pressKey(e.key, codeFromKey(false, e.key));
-            e.preventDefault();
-            return false;
-        }
+
+    var keyMap = { "Backspace": "ArrowLeft" };
+    keyMap[vim.keys.move[0]] = "ArrowLeft";
+    keyMap[vim.keys.move[1]] = "ArrowDown";
+    keyMap[vim.keys.move[2]] = "ArrowUp";
+    keyMap[vim.keys.move[3]] = "ArrowRight";
+
+    if (e.key in keyMap) {
+        e.key = keyMap[e.key];
+    }
+
+    if (e.key.indexOf("Arrow") == 0 || e.key == "Delete") {
+        docs.pressKey(e.key, docs.codeFromKey(false, e.key), true);
+    }
+
+    if (e.key == "V" && e.shift) {
+        console.log("select");
     }
 
     e.preventDefault();
     return false;
-}
+};
 
-function setBorderWidth(width) {
-    $("head").append("<style>.kix-cursor-caret { border-width: " + width + "; }</style>");
-}
-
+// This is what gets called when the user is in insert mode
 var lastChr = "";
 docs.keyboard.handleKeydown = function (e) {
     var chr = e.key; //"a", "b", etc.
     var escapeSeq = "hn";
-    if (mode == "insert") {
-        if (chr == "Escape") {
-            mode = "normal";
-            setBorderWidth("7px");
-            docs.keyboard.startBlockingKeyboard();
-        }
-        if (chr == escapeSeq[1] && lastChr == escapeSeq[0]) {
-            mode = "normal";
-            setBorderWidth("7px");
-            e.preventDefault();
-            docs.pressKey(e.key, codeFromKey(false, "Backspace"));
-            docs.keyboard.startBlockingKeyboard();
-            return false;
-        }
+
+    if (vim.mode != "insert") {
+        console.log("This shouldn't happen");
+        return true;
     }
-    if (mode == "normal") {
-        if (chr == "i") {
-            mode = "insert";
-            setBorderWidth("2px");
-            docs.keyboard.stopBlockingKeyboard();
-        }
+
+    if (chr == "Escape") {
+        vim.switchToNormalMode();
+    }
+    if (chr == vim.keys.escapeSeq[1] && lastChr == vim.keys.escapeSeq[0]) {
+        e.preventDefault();
+        // We need to delete the first character already typed in the seq.
+        docs.pressKey(e.key, docs.codeFromKey(false, "Backspace"), false);
+        vim.switchToNormalMode();
+        return false;
     }
     lastChr = chr;
-}
+};
